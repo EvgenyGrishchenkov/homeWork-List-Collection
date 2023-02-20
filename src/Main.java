2c9366b2c4349ced8cdff963619a34851da54a94
EvgenyGrishchenkov <smp_mzmo@mail.ru>
2023-02-20 11:05:20 +0300

    Использован Extract method

--- src/Main.java
+++ src/Main.java
@@ -23,72 +23,70 @@ public class Main {
 
             switch (numberOperation) {
                 case 1:
-                    System.out.println("Какую покупку хотите добавить?");
-                    String addItem = scanner.nextLine();
-                    list.add(addItem);
-                    System.out.println("Итого покупок в списке " + list.size());
+                    addProduct(scanner, list);
                     break;
                 case 2:
-                    System.out.println("Список покупок:");
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + ". " + list.get(i));
-                    }
-                    System.out.println("Какую покупку хотите удалить? Введите номер или название");
+                    shoppingList("Список покупок:", list);
 
-                    String removeItem = scanner.nextLine();
-                    try {
-                        int numberItem = Integer.parseInt(removeItem);
-                        System.out.println("Покупка " + list.get(numberItem - 1) + " удалена");
-                        list.remove(numberItem - 1);
-                    } catch (NumberFormatException e) {
-                        for (int i = 0; i< list.size(); i++) {
-                            if (removeItem.equals(list.get(i))) {
-                                System.out.println("Покупка " + removeItem + " удалена");
-                                list.remove(i);
-                            }
-                        /*for (String controlItem : list) { //через foreach не получилось :(
-                            if (controlItem.equals(removeItem)) {
-                                System.out.println("Покупка " + removeItem + " удалена");
-                                list.remove(controlItem);
-                            } else {
-                                System.out.println("Такой покупки нет");
-                            }*/
+                    removeProduct(scanner, list);
 
-                        }
-
-                    }
-                    System.out.println("Список покупок: ");
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + ". " + list.get(i));
-                    }
+                    shoppingList("Список покупок: ", list);
                     break;
                 case 3:
-                    System.out.println("Список покупок:");
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + ". " + list.get(i));
-                    }
+                    shoppingList("Список покупок:", list);
                     break;
 
                 case 4:
-                    System.out.println("Введите текст для поиска:");
-                    String query = scanner.nextLine();
-                    String queryLower = query.toLowerCase();
-                    for (int i = 0; i < list.size(); i++) {
-                        String item = list.get(i);
-                        String itemLower = item.toLowerCase();
-                        if (itemLower.contains(queryLower)) {
-                            System.out.println((i + 1) + ". " + list.get(i));
-                        }
-                    }
+                    searchProduct(scanner, list);
                     break;
 
                 default:
                     System.out.println("Такой операции нет");
-
             }
-
         }
-
     }
 
+    private static void searchProduct(Scanner scanner, List<String> list) {
+        System.out.println("Введите текст для поиска:");
+        String query = scanner.nextLine();
+        String queryLower = query.toLowerCase();
+        for (int i = 0; i < list.size(); i++) {
+            String item = list.get(i);
+            String itemLower = item.toLowerCase();
+            if (itemLower.contains(queryLower)) {
+                System.out.println((i + 1) + ". " + list.get(i));
+            }
+        }
+    }
+
+    private static void removeProduct(Scanner scanner, List<String> list) {
+        System.out.println("Какую покупку хотите удалить? Введите номер или название");
+        String removeItem = scanner.nextLine();
+        try {
+            int numberItem = Integer.parseInt(removeItem);
+            System.out.println("Покупка " + list.get(numberItem - 1) + " удалена");
+            list.remove(numberItem - 1);
+        } catch (NumberFormatException e) {
+            for (int i = 0; i< list.size(); i++) {
+                if (removeItem.equals(list.get(i))) {
+                    System.out.println("Покупка " + removeItem + " удалена");
+                    list.remove(i);
+                }
+            }
+        }
+    }
+
+    private static void addProduct(Scanner scanner, List<String> list) {
+        System.out.println("Какую покупку хотите добавить?");
+        String addItem = scanner.nextLine();
+        list.add(addItem);
+        System.out.println("Итого покупок в списке " + list.size());
+    }
+
+    private static void shoppingList(String x, List<String> list) {
+        System.out.println(x);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i));
+        }
+    }
 }
